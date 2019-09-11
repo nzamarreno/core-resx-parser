568f08d09eb74e8a8e3ac49bbf5388cf77c8ebaf
Nicolas Zamarreno <nzamarreno@talentsoft.com>
2019-09-11 06:59:42 +0200

    Add values in double

--- index.js
+++ index.js
@@ -2,207 +2,235 @@ const fs = require("fs");
 const XLSX = require("xlsx");
 const fetch = require("node-fetch");
 const xmlpoke = require('xmlpoke');
-const xml2js = require('xml2js'); 
-const parser = new xml2js.Parser();
+const xml2js = require('xml2js');
 const args = require('yargs').argv;
+const parser = new xml2js.Parser();
 
 // Get params CLI
 const PATH = args.path;
 const EXCEL = args.excel;
+const INSERT = args.insert;
 
-const API_KEY_TRANSLATE = "";
-const promisesFiles = [];
+// API Key Microsoft Translator (Key is in Azure Portal)
+const API_URL_TRANSLATOR = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=en";
+const API_KEY_TRANSLATOR = "6c1550e2be0e423290dee12dfde8d2af";
 
-if (PATH) {
-    fs.readdir(`${PATH}`, (_, files) => {
-        files.forEach(file => {
-            if (file.match(/resx$/) === null) return;
-            promisesFiles.push(
-                new Promise((resolveGroupBy, _) => {
-                    fs.readFile(__dirname + `/${PATH}/${file}`, function(err, data) {
-                        parser.parseString(data, function (err, result) {
-                            const resxObject = {};
-                            
-                            result.root.data.forEach(x => {
-                                if (x.value === undefined) {
-                                    console.log("missing value: ", x["$"].name, file)
-                                }
-                                resxObject[`${x["$"].name}`] = x.value ? x.value[0] : "";
-                            });
-                            resolveGroupBy({ title: `${file}`, resx: resxObject});
-                        });
-                    });
+if (!PATH) {
+    console.log("\x1b[41m", "ERROR: THE PATH OF THE FOLDER CONTAINING THE .RESX FILES SHOULD BE PROVIDED", "\x1b[0m");
+    return;
+}
+
+fs.readdir(`${PATH}`, (_, files) => {
+
+    const filesInFolder = files
+        .filter(resxFile => resxFile.match(/resx$/) !== null)
+        .map(resxFile => getPromiseWithParsedResx(resxFile));
+
+    Promise.all(filesInFolder).then(resxContentByFile => {
+        const filesByName = getFilesByName(resxContentByFile);
+        filesByName.forEach(fileGroup => {
+            const allKeys = [];
+            fileGroup.forEach(resxFile => {
+                for (key in resxFile.resx) {
+                    if (!allKeys.find(x => x === key)) allKeys.push(key);
+                }
+            });
+
+            const valuesByKey = [];
+            const prefixLangages = getPrefixLanguages(fileGroup.map(x => x.title));
+
+            allKeys.forEach(key => {
+                values = {};
+                values["key"] = key;
+
+                prefixLangages.forEach((lang, index) => {
+                    values[lang] = fileGroup[index].resx[key];
                 })
-            )
+                valuesByKey.push(values);
+            });
+            translateValues(getFileTitle(fileGroup[0].title), valuesByKey);
+            if (keysValueTwice.length > 0) createExcel("double-value", keysValueTwice);
         })
+    })
+});
 
-        Promise.all(promisesFiles).then(valueResx => {
-            const individualName = getTypeFile(valueResx);
-            const groupByTypes = groupFilesByCategory(individualName, valueResx);
-            groupByTypes.forEach(async (group) => {
-                const keys = [];
-                group.forEach(elementOfGroup => {
-                    for (key in elementOfGroup.resx) {
-                        if (!keys.find(x => x === key)) keys.push(key);
+function getPromiseWithParsedResx(resxFile) {
+    return new Promise((resolve, _) => {
+        fs.readFile(`${PATH}/${resxFile}`, function (err, fileContent) {
+            parser.parseString(fileContent, function (err, parsedContent) {
+                const resxObject = {};
+                const allKeys = [];
+
+                // RESX path <root> => <data>
+                parsedContent.root.data.forEach(dataObject => {
+                    if (dataObject.value === undefined) {
+                        console.log("Missing value: ", dataObject["$"].name, resxFile);
+                        return;
                     }
+                    allKeys.push(`${dataObject["$"].name}`);
+                    resxObject[`${dataObject["$"].name}`] = dataObject.value[0];
                 });
 
-                const datasToExtract = [];
-                const prefixs = getPrefixLanguage(group.map(x => x.title));
-                keys.forEach(key => {
-                    values = {};
-                    values["key"] = key;
+                getDouble(allKeys, `${resxFile}`);
 
-                    prefixs.forEach((lang, index) => {
-                        values[lang] = group[index].resx[key];
-                    })
-                    datasToExtract.push(values);
-                });
-                
-                await translateValues(getTitleFile(group[0].title), datasToExtract);
-            })
-        })
-    });
-} else {
-    console.log("\x1b[41m", "WARNING, YOU SHOULD PUT THE PATH OF YOUR RESOURCES WITH YOUR .RESX", "\x1b[0m")
+                resolve({ title: `${resxFile}`, resx: resxObject });
+            });
+        });
+    })
 }
 
-const URL_API = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=en";
-function createTranslateParams(prefixLang) {
-    return `${URL_API}&to=${prefixLang.join("&to=")}`;
+
+/**
+ * Get Values in double
+ * @param {Array} keys 
+ * @param {string} title 
+ */
+let keysValueTwice = [];
+function valueIsAlreadyPresent(array, what) {
+    return array.filter(item => item == what).length;
 }
 
-async function translateValues(_title, _values) {
-    const resultToSend = [];
-    const valNull = _values.filter(x => {
-        for (let el in x) {
-            if (x[el] === undefined) {
-                return x;
-            }
+function getDouble(allKeys, titleFile) {
+    // {key: string, count: number}
+    const keysTwice = [];
+
+    // Verify is value has is present twice
+    allKeys.forEach(x => {
+        let countValue = valueIsAlreadyPresent(allKeys, x);
+        if (countValue > 1 && !keysTwice.some(value => value.key === x)) {
+            keysTwice.push({key: x, count: countValue});
         }
     });
-    valNull.map(async (key) => {
+
+    if (keysTwice.length > 0) {
+        keysTwice.forEach(x => {
+            keysValueTwice.push({title: titleFile, value: x.key, count: x.count})
+        });
+    }
+}
+
+/**
+ * Translate Values with Microsoft Translator API
+ * @param {string} title 
+ * @param {Array} valuesByKey 
+ * @return {void}
+ */
+async function translateValues(title, valuesByKey) {
+    const keysWithMissingTranslations = valuesByKey.filter(key => Object.keys(key).some(langage => key[langage] === undefined));
+
+    keysWithMissingTranslations.map(async key => {
         const reference = key["en-GB"];
         if (reference) {
-            const prefixToTranslate = [];
-            for (let el in key) {
-                if (key[el] === undefined) {
-                    prefixToTranslate.push(el);
-                }
-            }
+            const langagesToTranslate = Object.keys(key).filter(langage => key[langage] === undefined);
 
-            // Call API
-            const url = createTranslateParams(prefixToTranslate);
+            // Call Microsoft Translator API
+            const url = `${API_URL_TRANSLATOR}&to=${langagesToTranslate.join("&to=")}`;
             const result = await fetch(url, {
                 method: 'post',
-                body: JSON.stringify([{Text: reference}]),
+                body: JSON.stringify([{ Text: reference }]),
                 headers: {
-                    "Ocp-Apim-Subscription-Key": API_KEY_TRANSLATE, 
+                    "Ocp-Apim-Subscription-Key": API_KEY_TRANSLATOR,
                     "Content-Type": "application/json"
                 }
             });
             const response = await result.json();
 
-            //FIXME: Improve how map values
-            for (let el in key) {
-                if (key[el] === undefined) {
-                    const includeTo = response[0].translations.find(x => el.includes(x.to));
-                    if (includeTo) key[el] = includeTo.text;
-                    xmlpoke(`${PATH}/${_title}.${el}.resx`, function(xml) {
+            langagesToTranslate.map(langage => {
+                const includeTo = response[0].translations.find(translation => langage.includes(translation.to));
+                if (includeTo) key[langage] = includeTo.text;
+
+                // Modify Resx and Insert value
+                if (INSERT) {
+                    xmlpoke(`${PATH}/${title}.${langage}.resx`, function (xml) {
                         xml
-                        .ensure(`root/data[@name='${key.key}']`)
-                        .setOrAdd(`root/data[@name='${key.key}']/value`, includeTo.text);
+                            .ensure(`root/data[@name='${key.key}']`)
+                            .setOrAdd(`root/data[@name='${key.key}']/value`, includeTo.text);
                     })
                 }
-            }
-
-            resultToSend.push(key);
+            });
         }
 
         const valuesCompleted = [];
-        _values.forEach(value => {
-            const indexValue = _values.findIndex(x => x.key === value.key);
+        valuesByKey.forEach(value => {
+            const indexValue = valuesByKey.findIndex(x => x.key === value.key);
             if (indexValue) {
-                _values[indexValue] = value;
+                valuesByKey[indexValue] = value;
             }
             valuesCompleted.push(value);
         })
 
         if (EXCEL) {
-            createExcel(_title, _values);
+            createExcel(title, valuesCompleted);
         }
-    }) 
+    })
+
+    if (EXCEL) {
+        createExcel(title, valuesByKey);
+    }
 }
 
 /**
- * 
- * @param {String} _nameFile 
- * @param {Array} _datas 
+ * Create Excel File
+ * @param {string} nameFile 
+ * @param {Array} datas 
+ * @return {void}
  */
-function createExcel(_nameFile, _datas) {
-    const ws = XLSX.utils.json_to_sheet(_datas);
+function createExcel(nameFile, datas) {
+    const ws = XLSX.utils.json_to_sheet(datas);
 
     const wb = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, "translate");
 
-    XLSX.writeFile(wb, `${PATH}/${_nameFile}.xlsx`);
+    XLSX.writeFile(wb, `${PATH}/${nameFile}.xlsx`);
 }
 
 /**
- * 
- * @param {Array - String} allResxFiles 
+ * Get code languages 
+ * @param {string[]} allResxFiles
+ * @return {string[]} 
  */
-function getPrefixLanguage(_allResxFiles) {
-    const prefixLang = []
-    _allResxFiles.forEach(file => {
-        nameFilesSplitted = file.split(".");
-        prefixLang.push(nameFilesSplitted[nameFilesSplitted.length - 2]);
-    })
-
-    return prefixLang;
+function getPrefixLanguages(allResxFiles) {
+    return allResxFiles.map(file => {
+        const nameFilesSplitted = file.split(".");
+        return nameFilesSplitted[nameFilesSplitted.length - 2];
+    });
 }
 
 /**
- * 
- * @param {Array - String} _files 
+ * Get Array of file name 
+ * present in the folder without suffix and extension
+ * Example: your folder contain: translate.en-GB.resx / translate.fr-CA.resx / common.fr-CA.resx...
+ * Return Array [translate, common]
+ * @param {string[]} files
+ * @return {string[]} 
  */
-function getTypeFile(_files) {
-    const filesName = _files.map(x => x.title);
-    const uniq = []
-    filesName.map(file => {
-        uniq.push(getTitleFile(file))
-    })
+function getFilesName(files) {
+    const fileNames = files.map(file => getFileTitle(file.title));
 
-    return uniq.filter((item, pos) => {
-        return uniq.indexOf(item) == pos;
+    return fileNames.filter((item, pos) => {
+        return fileNames.indexOf(item) == pos;
     })
 }
 
 /**
- * 
- * @param {String} _file 
+ * Get only Title of file
+ * Without suffix of language and file extension
+ * @param {string} file 
+ * @return {string}
  */
-function getTitleFile(_file) {
-    const splittedFile = _file.split(".");
-    // Cut extensions file and Prefix language
-    splittedFile.splice(-1, 1);
-    splittedFile.splice(-1, 1);
-
-    return splittedFile.join(".");
+function getFileTitle(file) {
+    return file.split(".").slice(0, 2).join(".");
 }
 
 /**
- * 
- * @param {Array} _nameUniqFiles 
- * @param {Array} _files 
+ * Get Array with title and key => value GroupBy by FileName
+ * Example: your folder contain: translate.en-GB.resx / translate.fr-CA.resx / common.fr-CA.resx...
+ * Return Array [[{title: translate.en-GB.resx, resx: Object}, {title: translate.fr-CA.resx, resx: Object}], 
+ * [{title: common.fr-CA.resx, resx: Object}]]
+ * @param {string[]} files 
+ * @return {string[]}
  */
-function groupFilesByCategory(_nameUniqFiles, _files) {
-    const filesGroupBy = [];
-    _nameUniqFiles.forEach(f => {
-        let groupBy = []
-        _files.forEach(x => { if (x.title.startsWith(f)) groupBy.push(x) });
-        filesGroupBy.push(groupBy);
-    })
-
-    return filesGroupBy;
+function getFilesByName(files) {
+    const filesName = getFilesName(files);
+    return filesName.map(uniqueName => files.filter(file => file.title.startsWith(uniqueName)))
 }
